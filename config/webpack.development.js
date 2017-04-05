beef2cb9bbb8ee2ad1c6c11d19f4c4b792d86d99
jvhoven <jeffreyvanhoven@gmail.com>
2017-04-05 17:10:29 +0200

    Mock-up of login page

---
+++ config/webpack.development.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack')
+const path = require('path')
+const cwd = require('process').cwd()
+const Visualizer = require('webpack-visualizer-plugin')
+
+module.exports = {
+  devtool: 'cheap-module-source-map',
+  entry: {
+    client: path.resolve(cwd, 'app', 'index.js'),
+    vendor: path.resolve(cwd, 'app', 'vendor.js')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(cwd, 'dist'),
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: './app',
+    historyApiFallback: true, // for use with client side router
+    hot: false,
+    port: 3000
+  }
+}
