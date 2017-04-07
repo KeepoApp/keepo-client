5378f99faa048f66057dbb6e3e199a60e50c54ba
jvhoven <jeffreyvanhoven@gmail.com>
2017-04-07 10:57:31 +0200

    Add assets to express static folders

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const app = express();
 app.use('/dist', express.static('dist'));
+app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './index.html'));
