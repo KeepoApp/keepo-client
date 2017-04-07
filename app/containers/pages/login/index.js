3dc04ed1a074c00b2b2cc6bf16a12bb3e6454932
jvhoven <jeffreyvanhoven@gmail.com>
2017-04-07 20:24:15 +0200

    React 16-alpha7 🎉🎉, improved login

--- app/containers/pages/login/index.js
+++ app/containers/pages/login/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import Box, { VBox, Center } from 'react-layout-components';
 
 import './login.less';
 
 const Login = () => (
-  <VBox className="login">
-    <Center className="login__box login__box--form">
-      <Box>
+  <div className="login flex-center">
+    <div className="login__box">
+      <div className="box__logo flex-center">
         <img src="/assets/img/logo.png" alt="" />
-      </Box>
-      <Box className="login__box login__box--social">
+        <div className="divider" />
+      </div>
+      <div className="box__social">
         <h2>Login</h2>
         <p>
           Give thanks to the most high. Stay focused.
           Always remember in the jungle there’s a lot of they in there.
         </p>
-        <Box>
-          <div className="g-signin2" data-onsuccess="onSignIn" />
-        </Box>
-      </Box>
-    </Center>
-    <Center className="login__box login__box--love">
+        <div className="g-signin2" data-onsuccess="onSignIn" />
+      </div>
+    </div>
+    <div className="login__love">
       <p>Made with <i className="fa fa-heart" /> by Wesley de Bruijn and Jeffrey van Hoven</p>
-    </Center>
-  </VBox>
+    </div>
+  </div>
 );
 
 export default Login;
